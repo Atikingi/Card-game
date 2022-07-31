c260c5ed88c03fd58cdb76514859f98ac8353312
Atikingi <n.igumentsev@gmail.com>
2022-07-31 19:58:12 +0300

    hw2

--- src/game.js
+++ src/game.js
@@ -6,6 +6,35 @@ if (!sessionStorage.getItem('status')) {
 
 window.game = {
   status: sessionStorage.getItem('status'),
+  difficulty: '1',
 };
 
 const renderScreens = new GameRender();
+
+class GameEvent {
+  constructor() {
+    document.getElementById('start-game').addEventListener('click', (event) => {
+      event.preventDefault();
+
+      this.checkDifficulty(event);
+    });
+  }
+
+  checkDifficulty(event) {
+    const { target } = event;
+    const difficultyField = document.getElementById('difficulty-field');
+
+    for (let i = 0; i < difficultyField.children.length; i++) {
+      if (
+        difficultyField.children[i].type === 'radio' &&
+        difficultyField.children[i].checked
+      ) {
+        game.difficulty = difficultyField.children[i].value;
+      }
+    }
+
+    renderScreens.startGame(target);
+  }
+}
+
+const gameEvent = new GameEvent();
