634a7bf55d71b0d2bc29e1ac858a84032b8b9218
Atikingi <n.igumentsev@gmail.com>
2022-08-02 22:19:51 +0300

    add linter and formatter

--- src/game.js
+++ src/game.js
@@ -1,37 +1,39 @@
 import GameRender from '/src/render.js';
 
 if (!sessionStorage.getItem('status')) {
-  sessionStorage.setItem('status', 'start');
+   sessionStorage.setItem('status', 'start');
 }
 
 window.game = {
-  status: sessionStorage.getItem('status'),
-  difficulty: '1',
+   status: sessionStorage.getItem('status'),
+   difficulty: '1',
 };
 
 const renderScreens = new GameRender();
 
 class GameEvent {
-  constructor() {
-    document.getElementById('start-game').addEventListener('click', (event) => {
-      event.preventDefault();
+   constructor() {
+      document
+         .getElementById('start-game')
+         .addEventListener('click', (event) => {
+            event.preventDefault();
 
-      this.checkDifficulty(event);
-    });
-  }
+            this.checkDifficulty(event);
+         });
+   }
 
-  checkDifficulty(event) {
-    const { target } = event;
-    const difficultyField = document.getElementById('difficulty-field');
+   checkDifficulty(event) {
+      const { target } = event;
+      const difficultyField = document.getElementById('difficulty-field');
 
-    difficultyField.childNodes.forEach((node) => {
-      if (node.type === 'radio' && node.checked) {
-        game.difficulty = node.value;
-      }
-    });
+      difficultyField.childNodes.forEach((node) => {
+         if (node.type === 'radio' && node.checked) {
+            window.game.difficulty = node.value;
+         }
+      });
 
-    renderScreens.startGame(target);
-  }
+      renderScreens.startGame(target);
+   }
 }
 
-const gameEvent = new GameEvent();
+new GameEvent();
--- src/render.js
+++ src/render.js
@@ -1,31 +1,31 @@
 export default class GameRender {
-  container = document.getElementById('container');
+   container = document.getElementById('container');
 
-  constructor() {
-    this.checkGameStatus();
-  }
+   constructor() {
+      this.checkGameStatus();
+   }
 
-  checkGameStatus() {
-    if (game.status === 'start') {
-      this.renderScreen('template-difficulty');
+   checkGameStatus() {
+      if (window.game.status === 'start') {
+         this.renderScreen('template-difficulty');
 
-      this.difficultyForm = document.getElementById('difficulty-form');
-      this.difficultyForm.classList.remove('game__difficulty__hidden');
-    }
-  }
+         this.difficultyForm = document.getElementById('difficulty-form');
+         this.difficultyForm.classList.remove('game__difficulty__hidden');
+      }
+   }
 
-  renderScreen(templateName) {
-    const templateItem = document.getElementById(templateName);
-    const clone = templateItem.content.cloneNode(true);
+   renderScreen(templateName) {
+      const templateItem = document.getElementById(templateName);
+      const clone = templateItem.content.cloneNode(true);
 
-    this.container.appendChild(clone);
-  }
+      this.container.appendChild(clone);
+   }
 
-  startGame() {
-    this.difficultyForm.classList.add('game__difficulty__hidden');
+   startGame() {
+      this.difficultyForm.classList.add('game__difficulty__hidden');
 
-    game.status = 'game';
+      game.status = 'game';
 
-    this.renderScreen('template-game');
-  }
+      this.renderScreen('template-game');
+   }
 }
